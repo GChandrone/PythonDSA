d16b4f083bec6e8a24385aa56f794db04b47de4a
Gabriel da Cunha <gabriel.cunhagabi@gmail.com>
2023-10-11 23:22:18 -0300

    atualizando versao do jogo

--- Cap07/jogodaforca_v1.py
+++ Cap07/jogodaforca_v1.py
@@ -12,6 +12,83 @@ def limpatela():
     else:
         system('clear')
 
+# Função que desenha a forca na tela
+def desenho_boneco(chances):
+
+    # Lista de estágios da forca
+    estagios = [ # estágio 6 (final)
+     """
+        --------
+        |      |
+        |      O
+        |     \\|/
+        |      |
+        |     / \\
+        -
+     """,
+     # estágio 5
+     """
+        --------
+        |      |
+        |      O
+        |     \\|/
+        |      |
+        |     / 
+        -
+     """,
+     # estágio 4
+     """
+        --------
+        |      |
+        |      O
+        |     \\|/
+        |      |
+        |      
+        -
+     """,
+     # estágio 3
+     """
+        --------
+        |      |
+        |      O
+        |     \\|
+        |      |
+        |     
+        -
+     """,
+     # estágio 2
+     """
+        --------
+        |      |
+        |      O
+        |      |
+        |      |
+        |     
+        -
+     """,
+     # estágio 1
+     """
+        --------
+        |      |
+        |      O
+        |    
+        |      
+        |     
+        -
+     """,
+     # estágio 0
+     """
+        --------
+        |      |
+        |      
+        |    
+        |      
+        |     
+        -
+     """
+    ]
+    return estagios[chances]
+
 # Função Principal
 def game():
 
@@ -21,13 +98,13 @@ def game():
     print("Adivinhe a palavra abaixo:\n")
 
     # Lista de palavras para o jogo
-    palavras = ['Banana', 'Uva', 'Morango', 'Laranja', 'Abacate', 'manga']
+    palavras = ['Banana', 'Uva', 'Morango', 'Laranja', 'Abacate', 'Manga']
    
     # Escolhe aleatoriamente uma palavra
     palavra = random.choice(palavras) 
     
     # List comprehension
-    qtd_= ['_' for letra in palavra]
+    letras_descobertas = ['_' for letra in palavra]
 
     # Número de chances
     chances = 6
@@ -35,4 +112,52 @@ def game():
     # Lista de palavras erradas
     letras_erradas = []
 
-game()
+    frase = " "
+
+    while chances > 0:
+
+        limpatela()
+
+        # Print
+        print(desenho_boneco(chances))
+        print(" ".join(letras_descobertas))
+        print("\nChances restantes:", chances)
+        print("Letras erradas:", " ".join(letras_erradas))
+        print("\n",frase)
+        # Tentativa
+        tentativa = input("\nDigite uma letra: ").lower()
+
+        # Condicional
+        if tentativa in palavra.lower():
+            index = 0
+
+            for letra in palavra:
+                if tentativa == letra.lower():
+                    letras_descobertas[index] = letra
+                    if tentativa in letras_descobertas and tentativa in letras_erradas:
+                        frase = "Está letra já foi digitada"
+                index += 1
+        else:
+            chances -= 1
+            letras_erradas.append(tentativa)
+
+        # Condicional
+        if "_" not in letras_descobertas:
+            limpatela()
+            print(desenho_boneco(chances))
+            print(" ".join(letras_descobertas))
+            print("\nParabéns você venceu!")
+            print("\nChances restantes:", chances)
+            print("Letras erradas:", " ".join(letras_erradas))
+            break
+            
+    # Condicional        
+    if "_" in letras_descobertas:
+        limpatela()
+        print(desenho_boneco(chances))
+        print("\nVocê Perdeu, a palavra era", palavra)
+
+# Bloco Main
+if __name__ == "__main__":
+    game()
+    print("\nDeus me ajuda a aprender e entender tudo. Amém!!\n") 
